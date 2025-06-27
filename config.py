dd364d4396bf4b0ab71dc065849aa88c2e68281d
om <130356845+om22-12-86@users.noreply.github.com>
2025-06-27 11:58:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 ADMIN_IDS = [int(id.strip()) for id in os.getenv("ADMIN_IDS", "").split(',') if id.strip()]
-DB_NAME = os.getenv("DB_NAME", "botcanal").strip()
-DB_USER = os.getenv("DB_USER", "botcanal").strip()
-DB_PASSWORD = os.getenv("DB_PASSWORD", "botcanal").strip()
-DB_HOST = os.getenv("DB_HOST", "localhost").strip()
-DB_PORT = int(os.getenv("DB_PORT", 5432))
+DB_NAME = os.getenv("DB_NAME", "xxxxxx").strip()
+DB_USER = os.getenv("DB_USER", "xxxxx").strip()
+DB_PASSWORD = os.getenv("DB_PASSWORD", "xxxx").strip()
+DB_HOST = os.getenv("DB_HOST", "xxxxx").strip()
+DB_PORT = int(os.getenv("DB_PORT", xxxxx))
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
